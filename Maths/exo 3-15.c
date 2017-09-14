de7a0a7a8dc8487514f77326313c02dbc0b82495
ArthurDuneau <31952536+ArthurDuneau@users.noreply.github.com>
2017-09-14 10:28:11 +0200

    Add files via upload

---
+++ Maths/exo 3-15.c
@@ -0,0 +1,27 @@
+/*
+CONTENU      : Maths ex 3-15
+AUTEUR       : Arthur DUNEAU
+CREATION     : 22/09/2013
+MODIFICATION : 22/09/2013
+*/
+
+// librairies
+#include <stdio.h>
+#include <stdlib.h>
+
+float a,b,c,d;
+// corps du programme principal
+int main (void)
+{
+  printf("première note\n");
+  scanf("%f",&a);
+  printf("deuxième note\n");
+  scanf("%f",&b);
+  printf("troisième note\n");
+  scanf("%f",&c);
+  printf("quatrième note\n");
+  scanf("%f",&d);
+  
+
+    return 0;
+}
